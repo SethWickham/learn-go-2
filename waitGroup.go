6e26b624ec565dc103b549f5d7fd142d2a7907db
SethWickham <seth.wickham@hotmail.com>
2019-08-26 16:29:15 -0700

    added comments and Goroutines from runtime

--- waitGroup.go
+++ waitGroup.go
@@ -6,11 +6,20 @@ import (
 )
 
 func waitGroup() {
-
-	numberToZero()
-	numberToTen()
+	//here we have code that is running in Parrallel thanks to the use of this computer's multiple core processors
+	//go was created to take specific advantage of multiple core processor's and thus handles
+	//Concurrency and Parrallelism extremely well
+	fmt.Println("waitGroup Print START")
 
 	fmt.Println("Number of CPU's:", runtime.NumCPU())
+
+	fmt.Println("Goroutines", runtime.NumGoroutine())
+	go numberToZero()
+	go numberToTen()
+
+	fmt.Println("Goroutines", runtime.NumGoroutine())
+
+	fmt.Println("waitGroup Print END")
 }
 
 func numberToZero() {
