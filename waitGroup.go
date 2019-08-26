76479012de096a7dcb5dd808fe4cc1c2f0d203cc
SethWickham <seth.wickham@hotmail.com>
2019-08-26 16:50:14 -0700

    added WaitGroup code and comments

--- waitGroup.go
+++ waitGroup.go
@@ -3,21 +3,29 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func waitGroup() {
 	//here we have code that is running in Parrallel thanks to the use of this computer's multiple core processors
 	//go was created to take specific advantage of multiple core processor's and thus handles
 	//Concurrency and Parrallelism extremely well
 	fmt.Println("waitGroup Print START")
-
+	//this will show us how many processor's are available to us on our computer
 	fmt.Println("Number of CPU's:", runtime.NumCPU())
 
-	fmt.Println("Goroutines", runtime.NumGoroutine())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	//here we are launching new go routines (to take advantage of multiple core processors ) by using the word go before our functions
+	wg.Add(2)
 	go numberToZero()
 	go numberToTen()
+	fmt.Println("new number of Goroutines:", runtime.NumGoroutine())
+	wg.Wait()
 
-	fmt.Println("Goroutines", runtime.NumGoroutine())
+	//because our newly added (2) Goroutines have ended we are back to our one Goroutine which runs our func main, then when main exits our program shuts down
+	fmt.Println("Final number of Goroutines:", runtime.NumGoroutine())
 
 	fmt.Println("waitGroup Print END")
 }
@@ -26,7 +34,7 @@ func numberToZero() {
 	for i := 10; i >= 0; i-- {
 		fmt.Println("numberToZero:", i)
 	}
-
+	wg.Done()
 }
 
 func numberToTen() {
@@ -34,4 +42,5 @@ func numberToTen() {
 		fmt.Println("numberToTen:", i)
 
 	}
+	wg.Done()
 }
