19b7f8f7d189438d46d19c85caeb82580fa3de47
SethWickham <seth.wickham@hotmail.com>
2020-01-18 13:22:19 -0800

    added comments to directionalChannels.go

--- directionalChannels.go
+++ directionalChannels.go
@@ -4,16 +4,17 @@ import "fmt"
 
 func directionalChannels() {
 	fmt.Println("directionalChannels Print START")
-	//basic channel
+	//bidirectional channel -by default a channel can send and recieve
+	//but we can define a channel to only recieve or send see below
 	c := make(chan string)
 
 	go func() {
 		c <- "channel to pass VALUES of TYPE string"
 	}()
-	//receiving channel
+	//receive only channel
 	cr := make(<-chan string)
 
-	//send channel
+	//send only channel
 	cs := make(chan<- string)
 
 	//set VALUE that has been passed to channel to ch and then print VALUE
